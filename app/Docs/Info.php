bdbb8c8e310fa529f9b56ca3653ad716315ed141
a20688392 <a20688392@gmail.com>
2023-07-28 13:28:42 +0800

    docs: add swagger demo info

---
+++ app/Docs/Info.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Docs;
+
+/**
+ * @OA\Info(
+ *     version="1.0.0",
+ *     title="Board API Demo",
+ *     description="留言板API 文件",
+ * )
+ * @OA\PathItem(
+ *     path="/"
+ * )
+ */
+class Info
+{
+}
---
+++ app/Docs/Server.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Docs;
+
+/**
+ * @OA\Server(
+ *     description="API Demo Server",
+ *     url=L5_SWAGGER_CONST_HOST
+ * )
+ */
+class Server
+{
+}
